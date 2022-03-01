d1a751aaae5ce8b2765467d645f9019bce903f8b
Marcelo Iury <51893535+MarceloINS@users.noreply.github.com>
2022-03-01 13:04:02 -0300

    Add files via upload

--- AlgGeoComp/src/Main/LinearAlgebra.java
+++ AlgGeoComp/src/Main/LinearAlgebra.java
@@ -2,8 +2,8 @@ package Main;
 
 public class LinearAlgebra {
 
+    //Transposta
     public Matriz transpose(Matriz a) {
-
         Integer[][] elementos = new Integer[a.getColuna()][a.getLinha()];
         for (int i = 0; i < a.getLinha(); i++) {
             for (int j = 0; j < a.getColuna(); j++) {
@@ -13,4 +13,54 @@ public class LinearAlgebra {
         return new Matriz(a.getColuna(), a.getLinha(), elementos);
     }
 
+    //Somar
+    public Matriz sum(Matriz a, Matriz b) {
+        Integer[][] elementos = new Integer[a.getLinha()][a.getColuna()];
+        if(a.getLinha() == b.getLinha() && a.getColuna() == b.getColuna()) {
+            for(int i = 0; i < a.getLinha(); i++) {
+                for(int j = 0; j < a.getColuna(); j++) {
+                    elementos[i][j] = a.getElemento(i,j) + b.getElemento(i,j);
+                }
+            }
+        } else {throw new Error("As dimensoes dos operandos precisam ser iguais");}
+
+        if(a instanceof Vetor) {
+            return new Vetor(a.getLinha(), elementos);
+        } else {
+            return new Matriz(a.getLinha(), a.getColuna(), elementos);
+        }
+    }
+
+    //Times
+    public Matriz times(Matriz a, Matriz b) {
+        Integer[][] elementos = new Integer[a.getLinha()][a.getColuna()];
+        if(a.getLinha() == b.getLinha() && a.getColuna() == b.getColuna()) {
+            for(int i = 0; i < a.getLinha(); i++) {
+                for(int j = 0; j < a.getColuna(); j++) {
+                    elementos[i][j] = a.getElemento(i,j) * b.getElemento(i,j);
+                }
+            }
+        } else {throw new Error("As dimensoes dos operandos precisam ser iguais");}
+
+        if(a instanceof Vetor) {
+            return new Vetor(a.getLinha(), elementos);
+        } else {
+            return new Matriz(a.getLinha(), a.getColuna(), elementos);
+        }
+    }
+
+    public Matriz times(Matriz a, int b) {
+        Integer[][] elementos = new Integer[a.getLinha()][a.getColuna()];
+        for(int i = 0; i < a.getLinha(); i++) {
+            for(int j = 0; j < a.getColuna(); j++) {
+                elementos[i][j] = a.getElemento(i,j) * b;
+            }
+        }
+
+        if(a instanceof Vetor) {
+            return new Vetor(a.getLinha(), elementos);
+        } else {
+            return new Matriz(a.getLinha(), a.getColuna(), elementos);
+        }
+    }
 }
--- AlgGeoComp/src/Main/Main.java
+++ AlgGeoComp/src/Main/Main.java
@@ -10,7 +10,7 @@ public class Main {
         LinearAlgebra linearAlgebra = new LinearAlgebra();
         AuxMatriz auxMatriz = new AuxMatriz();
 
-        Matriz matriz = auxMatriz.createMatrix(linha, coluna);
+        /*Matriz matriz = auxMatriz.createMatrix(linha, coluna);
 
         auxMatriz.exibir(matriz);
 
@@ -20,5 +20,35 @@ public class Main {
         Matriz a = linearAlgebra.transpose(matriz);
 
         auxMatriz.exibir(a);
+
+        System.out.println();
+        System.out.println();*/
+
+
+        Matriz b = auxMatriz.createMatrix(linha, coluna);
+        auxMatriz.exibir(b);
+
+        System.out.println();
+        System.out.println();
+
+        Matriz c = auxMatriz.createMatrix(linha, coluna);
+        auxMatriz.exibir(c);
+
+        System.out.println();
+        System.out.println();
+
+        //auxMatriz.exibir(linearAlgebra.sum(b,c));
+
+        System.out.println();
+        System.out.println();
+
+        auxMatriz.exibir(linearAlgebra.times(b, 2));
+
+        System.out.println();
+        System.out.println();
+
+        auxMatriz.exibir(linearAlgebra.times(b, c));
+
+
     }
 }
